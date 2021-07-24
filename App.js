80a6033412d463420fc96dcb44cddeaa1e7c0907
bill0x2A <billysmith1998@live.com>
2021-07-24 18:59:34 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import Navbar from './components/Navbar/Navbar';
+
+const { useState } = React;
 
 export default function App() {
+  let [counter, setCounter] = useState(0)
   return (
+    <React.Fragment>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Navbar></Navbar>
+      <Text>Counter App</Text>
+      <Text>{counter}</Text>
+      <Button
+        title="Increment Counter"
+        onPress={() => setCounter(counter + 1)}
+        style={styles.button}
+      />
+      <ActivityIndicator size="small" color="#0000ff" />
       <StatusBar style="auto" />
     </View>
+    </React.Fragment>
   );
 }
 
@@ -18,4 +32,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop : "100px",
+  }
 });
